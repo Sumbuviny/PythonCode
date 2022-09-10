6f89e30aff1d51f077763c6f1bfd729da9534586
sumbu viny <sumbuviny3@gmail.com>
2022-09-10 15:57:42 -0700

    Create addition.py
    -Prompt the user for input.
    -Adds 2 numbers.
    -Prints the result.

---
+++ addition.py
@@ -0,0 +1,7 @@
+# Prompt the user for input
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+# Adds number1 and number2
+sum = number1 + number2
+# prints the result
+print("The sum is", sum)
